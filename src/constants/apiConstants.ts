a5bc69435e8e5b0403d081c05ee56a123fb77d28
Yuvaraj I <yuvaraji@presidio.com>
2023-10-02 00:01:31 +0530

    Update response integration changes

--- src/constants/apiConstants.ts
+++ src/constants/apiConstants.ts
@@ -14,7 +14,11 @@ const CONFIG = {
   method: METHOD.post,
 };
 
+enum Language {
+  TS = "TypeScript",
+}
+
 const baseUrl: string =
   "https://10nozi0l51.execute-api.us-east-1.amazonaws.com/dev/";
 
-export { HEADERS, baseUrl, METHOD, CONFIG };
+export { HEADERS, baseUrl, METHOD, CONFIG, Language };
