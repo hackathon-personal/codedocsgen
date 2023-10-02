411f94b5f49c9daa7ff905f11b055e5c147b85e2
saipratap9 <skondreddy@presidio.com>
2023-10-03 00:16:02 +0530

    refactor: added inline comments

--- src/commands/generateDocsCommand.ts
+++ src/commands/generateDocsCommand.ts
@@ -16,47 +16,64 @@ const generateDocs = vscode.commands.registerCommand(
       const document = editor.document;
       const cursorPosition = editor.selection.active;
 
+      // entire ts file text till end of the file
       let textFromCursorToEnd = document.getText();
 
       if (textFromCursorToEnd) {
+        // parse the text to get the functions
         const parsedFunctions = tsParser.getFunction(textFromCursorToEnd);
         console.log("parsedFunctions", parsedFunctions);
 
         if (parsedFunctions.length > 0) {
           try {
+            // function to generate the comments
             let selectedFunc = [];
+            // selected function start line
             let selectedFuncStartPos: number | null = null;
-            for (const funcObj of parsedFunctions) {
-              const [startPos, endPos]: any = funcObj.functionName?.split("#");
+            // loop through the parsed functions to find the selected function
+            for (const functionDetails of parsedFunctions) {
+              // get the start and end position of the function
+              const [startPos, endPos]: any =
+                functionDetails.functionName?.split("#");
+              // check if the cursor position is within the function
               selectedFuncStartPos = parseInt(startPos);
               console.log(startPos, endPos, cursorPosition.line);
+              // check if the cursor position is within the function
               if (
                 parseInt(startPos) <= cursorPosition.line + 1 &&
                 cursorPosition.line + 1 <= parseInt(endPos)
               ) {
-                selectedFunc.push(funcObj);
+                // if yes then add the function to the selected function array
+                selectedFunc.push(functionDetails);
                 break;
               }
             }
             console.log(selectedFunc);
+            // check if the selected function is empty
             if (selectedFunc.length === 0) {
+              // if yes then show the info message
               vscode.window.showInformationMessage(
                 ERROR_MESSAGES.functionNotFound,
                 ACTIONS.dismiss
               );
               return;
             }
+
+            // get the response of JSDocs from the api
             let responseDocs = await apiService.getGenerateComment(
               selectedFunc
             );
             console.log(responseDocs);
 
+            // get the function name from the selected function
             responseDocs = responseDocs[selectedFunc[0].functionName!];
             responseDocs = `${responseDocs}${"\n"}`;
 
+            // insert the comments in the editor
             if (responseDocs) {
               editor.edit((editBuilder) => {
-                if (selectedFuncStartPos != null) {
+                if (selectedFuncStartPos !== null) {
+                  // get the start position of the selected function
                   let startPosition = cursorPosition.with(
                     selectedFuncStartPos - 1,
                     0
@@ -67,6 +84,7 @@ const generateDocs = vscode.commands.registerCommand(
                   }
 
                   console.log(startPosition);
+                  // insert the comments in the editor
                   editBuilder.insert(startPosition, responseDocs);
                 }
               });
--- src/commands/generateDocsForFileCommand.ts
+++ src/commands/generateDocsForFileCommand.ts
@@ -8,6 +8,56 @@ import {
   ERROR_MESSAGES,
 } from "../constants/applicationConstants";
 
+const generateDocsForFile = vscode.commands.registerCommand(
+  COMMANDS.GENERATE_DOCS_FOR_FILE,
+  async () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (editor) {
+      const document = editor.document;
+      // entire ts file text till end of the file
+      const documentText = document.getText();
+      const cursorPosition = editor.selection.active;
+
+      if (documentText) {
+        // parse the text to get the functions
+        const parsedFunctions = tsParser.getFunction(documentText);
+        console.log("parsedFunctions", parsedFunctions);
+        // check if the parsed functions are not empty
+        if (parsedFunctions.length > 0) {
+          try {
+            // generates JS doc comments for the functions passed
+            const generatedComments: FunctionDocsResponse =
+              await apiService.getGenerateComment(parsedFunctions);
+            console.log(generatedComments);
+
+            // check if the generated comments are not empty
+            if (
+              generatedComments &&
+              Object.keys(generatedComments).length > 0
+            ) {
+              // insert the comments in the file
+              await insertComments(generatedComments, editor, cursorPosition);
+            }
+          } catch (error) {
+            console.log("Error", error);
+            vscode.window.showErrorMessage(
+              ERROR_MESSAGES.somethingWentWrong,
+              ACTIONS.dismiss
+            );
+          }
+        } else {
+          // if empty then show the info message
+          vscode.window.showInformationMessage(
+            ERROR_MESSAGES.functionNotFound,
+            ACTIONS.dismiss
+          );
+        }
+      }
+    }
+  }
+);
+
 async function insertComments(
   generatedComments: any,
   editor: any,
@@ -15,15 +65,19 @@ async function insertComments(
 ) {
   let lineCount = 0;
 
+  // loop through the generated comments
   for (let line of Object.keys(generatedComments)) {
+    // get the start and end position of the function
     const [startPos, endPos] = line.split("#");
     const selectedFuncStartPos = parseInt(startPos);
     const selectedFuncEndPos = parseInt(endPos);
     const document = editor.document;
 
+    // get the generatedComment of the function
     let jSDocComment = generatedComments[line];
 
     await editor.edit((editBuilder: any) => {
+      // get the start position of the selected function
       let startPosition = cursorPosition.with(
         selectedFuncStartPos +
           (lineCount === 0 && selectedFuncStartPos > 0 ? -1 : lineCount),
@@ -39,52 +93,10 @@ async function insertComments(
       console.log(startPosition);
       editBuilder.insert(startPosition, jSDocComment);
     });
+    // increase the line count by the number of lines in the generated comment
     lineCount = lineCount + jSDocComment.split("\n").length - 1;
   }
 }
-
-const generateDocsForFile = vscode.commands.registerCommand(
-  COMMANDS.GENERATE_DOCS_FOR_FILE,
-  async () => {
-    const editor = vscode.window.activeTextEditor;
-
-    if (editor) {
-      const document = editor.document;
-      const documentText = document.getText();
-      const cursorPosition = editor.selection.active;
-
-      if (documentText) {
-        const parsedFunctions = tsParser.getFunction(documentText);
-        console.log("parsedFunctions", parsedFunctions);
-        if (parsedFunctions.length > 0) {
-          try {
-            const generatedComments: FunctionDocsResponse =
-              await apiService.getGenerateComment(parsedFunctions);
-            console.log(generatedComments);
-
-            if (
-              generatedComments &&
-              Object.keys(generatedComments).length > 0
-            ) {
-              await insertComments(generatedComments, editor, cursorPosition);
-            }
-          } catch (error) {
-            console.log("Error", error);
-            vscode.window.showErrorMessage(
-              ERROR_MESSAGES.somethingWentWrong,
-              ACTIONS.dismiss
-            );
-          }
-        } else {
-          vscode.window.showInformationMessage(
-            ERROR_MESSAGES.functionNotFound,
-            ACTIONS.dismiss
-          );
-        }
-      }
-    }
-  }
-);
 
 const generateDocsForFileCommand = {
   generateDocsForFile,
