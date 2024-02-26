e10d50316273e914d38042b5dc36b05d5adee218
Yuvaraj I <yuvaraji@presidio.com>
2024-02-26 23:15:52 +0530

    Update base url change

--- src/constants/apiConstants.ts
+++ src/constants/apiConstants.ts
@@ -19,6 +19,6 @@ enum Language {
 }
 
 const baseUrl: string =
-  "https://10nozi0l51.execute-api.us-east-1.amazonaws.com/dev/";
+  "https://vevwtutfsf.execute-api.us-east-1.amazonaws.com/dev";
 
-export { HEADERS, baseUrl, METHOD, CONFIG, Language };
+export { CONFIG, HEADERS, Language, METHOD, baseUrl };
